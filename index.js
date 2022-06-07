36ea4397d8b801ae0039b203bfed003ef82c8bcf
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-07 02:54:08 -0400

    commit tree ca8779834349c14456cf8846a4a8a9e535d91d5d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const writeCards = (names) => {
+  return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+
+const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+        console.log(i)
+    }
+}
